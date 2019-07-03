33cbc10cc592a6b02953103fafcefc20a1aefc49
Jonah Alligood <jonahisadev@gmail.com>
2019-07-02 21:26:45 -0500

    Version 1.1.1: Updated README and created API docs

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const scraper = require('./main');
+const scraper = require('./scraper');
 
 function printProduct(prod) {
 	console.log("The " + prod.name() + " is " + prod.priceStr());
@@ -9,5 +9,14 @@ function printProduct(prod) {
 // Full URL
 scraper.get("https://www.newegg.com/p/N82E16819113499", (prod) => printProduct(prod));
 
+// The AMD Ryzen 7 2700X is 306.38 USD
+// Image URL: https://c1.neweggimages.com/ProductImageCompressAll1280/19-113-499-V01.jpg
+// Rating: 5
+
+
 // Product ID
 scraper.get("N82E16814202237", (prod) => printProduct(prod));
+
+// The SAPPHIRE 100385L is 79.99 USD
+// Image URL: https://c1.neweggimages.com/ProductImageCompressAll1280/14-202-237-01.jpg
+// Rating: 4
